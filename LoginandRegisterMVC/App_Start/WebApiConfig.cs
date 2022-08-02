7d73f2035988d3589073f733c3cfc64715a9ba12
yuvachn <yuva10114@gmail.com>
2022-08-02 11:57:00 +0530

    api template

---
+++ LoginandRegisterMVC/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+
+namespace LoginandRegisterMVC.App_Start
+{
+   
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // TODO: Add any additional configuration code.
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+           
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+        }
+    }
+}
