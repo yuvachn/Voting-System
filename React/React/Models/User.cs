0f658f7420a7bdf648531dfbdc4512318f589cfa
yuvachn <yuva10114@gmail.com>
2022-08-06 21:39:55 +0530

    React api

---
+++ React/React/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace React.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public int Id { get; set; }
+
+        public int PhoneNo { get; set; }
+        //"{"Name"="Yuva","Email"="yuva @gmail.com","Id"="10","PhoneNo"="12345"}" 
+    }
+}
